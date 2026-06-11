9ab2dbb742876adc8938ff7cd6a9ba3f92ef45b6
dhvani-4125 <dhvanivanzwala2005@gmail.com>
2026-06-11 13:04:28 +0530

    Refactor SetDemo class to initialize set with user input

--- setops.py
+++ setops.py
@@ -1,13 +1,35 @@
 class SetDemo:
 
-    def create_set(self):
-        self.my_set = {10, 20, 30}
-        print("Set:", self.my_set)
+    def __init__(self):
+        n = int(input("Enter number of elements: "))
+        self.my_set = set()
+
+        for i in range(n):
+            value = input(f"Enter element {i + 1}: ")
+            self.my_set.add(value)
+
+    def display(self):
+        print(self.my_set)
 
     def add_element(self):
-        self.my_set.add(40)
-        print("After adding:", self.my_set)
+        value = input("Enter element to add: ")
+        self.my_set.add(value)
+        print(self.my_set)
 
     def remove_element(self):
-        self.my_set.remove(20)
-        print("After removing:", self.my_set)
+        value = input("Enter element to remove: ")
+        self.my_set.remove(value)
+        print(self.my_set)
+
+    def discard_element(self):
+        value = input("Enter element to discard: ")
+        self.my_set.discard(value)
+        print(self.my_set)
+
+    def pop_element(self):
+        self.my_set.pop()
+        print(self.my_set)
+
+    def clear_set(self):
+        self.my_set.clear()
+        print(self.my_set)
