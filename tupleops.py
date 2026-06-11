46c93a06b3009fdd1333663c52b3a1e39cf077a0
dhvani-4125 <dhvanivanzwala2005@gmail.com>
2026-06-11 13:05:25 +0530

    Implement TupleDemo class for tuple operations
    Replaced ListDemo class with TupleDemo class that handles tuple operations such as display, count, index, length, maximum, and minimum.

--- tupleops.py
+++ tupleops.py
@@ -1,13 +1,32 @@
-class ListDemo:
+class TupleDemo:
 
-    def create_list(self):
-        self.my_list = [10, 20, 30, 40]
-        print("List:", self.my_list)
+    def __init__(self):
+        n = int(input("Enter number of tuple elements: "))
 
-    def add_element(self):
-        self.my_list.append(50)
-        print("After adding:", self.my_list)
+        temp = []
 
-    def remove_element(self):
-        self.my_list.remove(20)
-        print("After removing:", self.my_list)
+        for i in range(n):
+            value = input(f"Enter element {i + 1}: ")
+            temp.append(value)
+
+        self.my_tuple = tuple(temp)
+
+    def display(self):
+        print(self.my_tuple)
+
+    def count_element(self):
+        value = input("Enter value to count: ")
+        print(self.my_tuple.count(value))
+
+    def index_element(self):
+        value = input("Enter value to find index: ")
+        print(self.my_tuple.index(value))
+
+    def length(self):
+        print(len(self.my_tuple))
+
+    def maximum(self):
+        print(max(self.my_tuple))
+
+    def minimum(self):
+        print(min(self.my_tuple))
